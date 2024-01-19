c07a3ed2134457fd6ef34c6b2442f924d83abe59
Chris Hancock <christopher.hancock0331@gmail.com>
2024-01-18 18:37:08 -0800

    add readmore feature to long text on about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,12 @@ import Image from 'next/image';
 const CrewWithTruck = "https://greenviewsolutionsimages.s3.us-west-1.amazonaws.com/AboutUs/AboutUsImage.jpg";
 
 const AboutUs = (props) => {
+  const [isTextExpanded, setTextExpanded] = useState({ paragraph1: false, paragraph2: false });
+
+  const toggleTextDisplay = (paragraph) => {
+    setTextExpanded(prevState => ({ ...prevState, [paragraph]: !prevState[paragraph] }));
+  };
+
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -38,6 +44,16 @@ const AboutUs = (props) => {
     };
   }, [])
 
+  const paragraph1 = " When you have a fence built and installed by the team at GreenView Solutions, you can rest assured that it is done so with extreme care and superior skill. We are a proud member of the American Fence Association and Master Builders Association, which means our superior skills haven’t gone unnoticed. Additionally, we participate in the Better Business Bureau Reliability Program, guaranteeing our company exhibits a reputation for excellent customer care. We are 100% bonded and insured, ensuring you can trust that our work will meet your standards of excellence and deliver a visually-appealing, purposeful fence for your home or business."
+
+  const paragraph2 = "Choosing your style of fencing is not an endeavor that you should take lightly. There is a lot to consider, including style, durability, area, and size. Because Denver and the surrounding communities have a vast assortment of plots and styles for residential neighborhoods and office complexes, GreenView Solutions offers a wide variety of fence styles to suit your needs perfectly. Not only do we utilize the best materials for all our fences, but we also employ a staff that uses proven techniques to ensure durability and elegance regardless of the scope of your project. We design and install wood fences, chain link fences, ornamental iron fences, specialty fences, and commercial fences, all with the grace and precision that can only be cultivated through nearly 50 years of hands-on experience."
+  
+  // Truncate text function
+  const truncateText = (text, length) => {
+    if (text.length <= length) return text;
+    return text.slice(0, length) + '...';
+  };
+
 
 
   return (
@@ -48,42 +64,36 @@ const AboutUs = (props) => {
         <h1 className={styles.title}>
           About Us
         </h1>
-        
+
         <div className={styles.aboutUsInfo}>
           <h3>We are Bonded and Insured</h3>
+          {/* First paragraph with toggle */}
           <p>
-            When you have a fence built and installed by the team at GreenView Solutions,
-            you can rest assured that it is done so with extreme care and superior skill.
-            We are a proud member of the American Fence Association and Master Builders Association,
-            which means our superior skills haven’t gone unnoticed. Additionally, we participate in
-            the Better Business Bureau Reliability Program, guaranteeing our company exhibits a reputation
-            for excellent customer care. We are 100% bonded and insured, ensuring you can trust that our work
-            will meet your standards of excellence and deliver a visually-appealing,
-            purposeful fence for your home or business.
+            {isTextExpanded.paragraph1 ? paragraph1 : truncateText(paragraph1, 250)}
+            <span className={styles.readMore} onClick={() => toggleTextDisplay('paragraph1')}>
+              {isTextExpanded.paragraph1 ? 'Read Less' : 'Read More'}
+            </span>
           </p>
 
+          {/* Second paragraph with toggle */}
           <h3>We Offer a Wide Range of Fencing Styles</h3>
+
           <p>
-            Choosing your style of fencing is not an endeavor that you should take
-            lightly. There is a lot to consider, including style, durability, area,
-            and size. Because Denver and the surrounding communities have a vast
-            assortment of plots and styles for residential neighborhoods and office
-            complexes, GreenView Solutions offers a wide variety of fence styles to suit
-            your needs perfectly. Not only do we utilize the best materials for all our
-            fences, but we also employ a staff that uses proven techniques to ensure
-            durability and elegance regardless of the scope of your project. We design and
-            install wood fences, chain link fences, ornamental iron fences, specialty fences,
-            and commercial fences, all with the grace and precision that can only be cultivated
-            through nearly 50 years of hands-on experience.
+            {isTextExpanded.paragraph2 ? paragraph2 : truncateText(paragraph2, 250)}
+            <span className={styles.readMore} onClick={() => toggleTextDisplay('paragraph2')}>
+              {isTextExpanded.paragraph2 ? 'Read Less' : 'Read More'}
+            </span>
           </p>
+
+
         </div>
 
 
         <div className={styles.imageAndDescription}>
-          
+
           <Image class={styles.crewImage} src={CrewWithTruck} alt='GreenView Solutions crew with truck' height={550} width={550} />
           <div className={styles.description}>
-            
+
             <div className={styles.aboutUsSection}>
               <div className={styles.aboutUsTitle}>
                 What Matters To Us:
@@ -129,4 +139,8 @@ const AboutUs = (props) => {
 
 
 export default AboutUs;
+
+
+
+
 
